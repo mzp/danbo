1177730d66b4e3621e1a4e09e0d489cc6a841382
mzp <mzpppp@gmail.com>
2011-07-15 11:08:46 +0900

    monkey patch

---
+++ ws_cli.rb
@@ -0,0 +1,154 @@
+# -*- coding: undecided -*-
+
+if !defined? MyTimer
+begin
+  require 'system_timer'
+  MyTimer = SystemTimer
+rescue LoadError
+  require 'timeout'
+  MyTimer = Timeout
+end
+end
+
+require 'socket'
+require 'openssl'
+require 'logger'
+require 'ws_proto_hybi07'
+require 'pp'
+
+
+class WSClient
+  def initialize(logger, options)
+    @logger = logger
+    @host = options.delete(:host)
+    @port = options.delete(:port)
+    @timeout = options.delete(:timeout) || 40
+    @version = options.delete(:proto) || :hybi07
+    @secure = options[:secure] if options.has_key?(:secure)
+    @proto = ProtoHybi07.new(@logger, options)
+    @extensions = []
+
+  end
+
+  def connect(url, protocols=[])
+    # make a handshake and send it to the server
+    handshake = @proto.make_handshake(@host, protocols, @extensions)
+    write_to_socket(handshake)
+    headers = read_http_headers
+    @proto.check_handshake_response(headers)
+    return true
+  end
+
+  def connect_raw(data)
+    write_to_socket(data)
+  end
+
+  def read_raw
+    @sock.read
+  end
+
+  def read_data
+    @proto.read_data(@sock)
+  end
+
+  def write_data(data, frame_size=nil)
+    @proto.send_text_data(@sock, data, frame_size)
+  end
+
+  def ping(text)
+    @proto.ping(@sock, text)
+  end
+
+  def close(code=nil, message=nil)
+    ret = @proto.close_connection(@sock, code, message)
+    @sock.close
+    return ret
+  end
+
+  private
+  Å£
+  def read_http_headers
+    headers = []
+    begin
+      line = @sock.readline.chomp
+#      puts "[#{line}] #{line.size}"
+      headers << line if line.size > 0
+    end while line != ""
+    headers
+  end
+
+  def with_sock
+    begin
+      MyTimer.timeout(@timeout) do
+        connect_i if @sock.nil?
+        yield @sock
+      end
+    rescue Exception => e
+      @sock.close if @sock
+      @sock = nil
+      return nil if e.is_a?(Timeout::Error)
+      @logger.error("exception #{e.message}")
+      return nil
+    end
+  end
+
+  def connect_to(host, port, timeout=nil)
+    puts
+    @logger.debug("host #{host} port #{port}")
+
+    if @secure
+      sock = TCPSocket.new(host, port)
+      ssl_context = OpenSSL::SSL::SSLContext.new()
+      puts "2DO add certificate verification"
+#      ssl_context.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      ssl_context.verify_mode = OpenSSL::SSL::VERIFY_PEER
+      ssl_context.ca_file = File.join(File.dirname(__FILE__), "cacert.pem")
+      sslsocket = OpenSSL::SSL::SSLSocket.new(sock, ssl_context)
+      sslsocket.sync_close = true
+      sslsocket.connect
+      @old_socket = sock
+      return sslsocket
+    else
+      addr = Socket.getaddrinfo(host, nil)
+      sock = Socket.new(Socket.const_get(addr[0][0]), Socket::SOCK_STREAM, 0)
+      if timeout
+        secs   = Integer(timeout)
+        usecs  = Integer((timeout - secs) * 1_000_000)
+        optval = [secs, usecs].pack("l_2")
+        sock.setsockopt Socket::SOL_SOCKET, Socket::SO_RCVTIMEO, optval
+        sock.setsockopt Socket::SOL_SOCKET, Socket::SO_SNDTIMEO, optval
+      end
+      sock.connect(Socket.pack_sockaddr_in(port, addr[0][3]))
+      return sock
+    end
+  end
+
+  def connect_i
+    @sock = connect_to(@host, @port, @timeout == 0 ? nil : @timeout)
+  end
+
+  def read_from_socket
+    @sock.read
+  end
+
+  def write_to_socket(cmd)
+    begin
+      connect_i if @sock.nil?
+      MyTimer.timeout(2) do
+        @sock.write(cmd)
+      end
+    rescue Exception => e
+      @logger.error("exception #{e.message}")
+      @sock.close if @sock
+      if @old_socket
+        @old_socket.close
+        @old_socket = nil
+      end
+      @sock = nil
+      return nil
+    end
+    return true
+  end
+
+
+end
